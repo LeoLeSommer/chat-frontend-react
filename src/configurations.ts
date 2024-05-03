4ae61ec03229944bdff69fcef5d8c3a0f23d2de0
Léo Le Sommer <llesommer.dev@gmail.com>
2024-05-03 07:23:03 +0200

    First commit

--- src/configurations.ts
+++ src/configurations.ts
@@ -3,15 +3,15 @@ import "dotenv/config";
 require("dotenv").config();
 
 const configurations = {
-  baseApiUrl: process.env.BASE_API_URL,
-  baseWebSocketUrl: process.env.BASE_WEBSOCKET_URL,
+  baseApiUrl: "http://localhost:9067",
+  baseWebSocketUrl: "http://localhost:9068",
   firebaseConfig: {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
+    apiKey: "AIzaSyCIVlZbKSnEUroxWiv0HJdN3kY_2uvrxV0",
+    authDomain: "chat-java-85cda.firebaseapp.com",
+    projectId: "chat-java-85cda",
+    storageBucket: "chat-java-85cda.appspot.com",
+    messagingSenderId: "215387889820",
+    appId: "1:215387889820:web:bad274d3079b97b2ed4d59",
   },
 };
 
